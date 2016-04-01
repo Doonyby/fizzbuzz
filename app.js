b31e371a15f1bbadb4462eb11f7a9e44b8bc16fe
Dan Riding <doonyby@gmail.com>
2016-04-01 17:42:38 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+$document.ready(function() {
+	$('#submitbutton').click(function() {
+		var result = $('#enternumber')
+
+	})
+})
+
+var jim = (i = 1; i <= result; i++)
+	if (jim % 15 = 0) {
+		document.write('fizzbuzz');
+	}
+	else if (jim % 3 = 0) {
+		$("#output").append('fizz');
+	}
+	else if (jim % 5 = 0) {
+		$("#output").append('buzz');
+	}
+	else {
+		$("#output").append(jim)
+	}
