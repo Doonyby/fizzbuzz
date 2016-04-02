90c71cd50e684b6860f6b902bbcb51da601aab7c
Dan Riding <doonyby@gmail.com>
2016-04-01 18:13:49 -0600

    error

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 $document.ready(function() {
 	$('#submitbutton').click(function() {
-		var result = $('#enternumber')
-
-	})
+		var result = $('#enternumber');
+		for (var i = 1; i <= result; i++) {
+			if (i % 15 === 0) {
+				$("#output").append('fizzbuzz');
+			}
+			else if (i % 3 === 0) {
+				$("#output").append('fizz');
+			}
+			else if (i % 5 === 0) {
+				$("#output").append('buzz');
+			}
+			else {
+				$("#output").append(jim)
+			}
+		};
+	});
 })
 
-var jim = (i = 1; i <= result; i++)
-	if (jim % 15 = 0) {
-		document.write('fizzbuzz');
-	}
-	else if (jim % 3 = 0) {
-		$("#output").append('fizz');
-	}
-	else if (jim % 5 = 0) {
-		$("#output").append('buzz');
-	}
-	else {
-		$("#output").append(jim)
-	}
