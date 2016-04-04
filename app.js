2aa4e513d71dbf34c3bb4c94b22bc4bbe3383d0c
Dan Riding <doonyby@gmail.com>
2016-04-04 11:25:02 -0600

    done except for reset

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ $(document).ready(function() {
 		var result = $('#enternumber').val();
 		for (var i = 1; i <= result; i++) {
 			if (i % 15 === 0) {
-				$("#output").append('fizzbuzz');
+				$("#output").append('<li>fizzbuzz</li>');
 			}
 			else if (i % 3 === 0) {
-				$("#output").append('fizz');
+				$("#output").append('<li>fizz</li>');
 			}
 			else if (i % 5 === 0) {
-				$("#output").append('buzz');
+				$("#output").append('<li>buzz</li>');
 			}
 			else {
-				$("#output").append(i)
+				$("#output").append('<li>' + i + '</li>');
 			}
 		};
+		$('#enternumber').val('');
+		$(this).reset();
 	});
-})
+});
 
